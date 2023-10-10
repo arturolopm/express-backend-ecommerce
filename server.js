5b96d36dc0d1f996e26ac796946b184298776c25
Arturo <carturolopezm@gmail.com>
2023-10-10 05:15:45 -0500

    Fixed routes

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-import Express from "express";
-import dotenv from "dotenv";
-import connectDatabase from "./config/MongoDB.js";
-import ImportData from "./DataImport.js";
-import productRouter from "./routes/Product.routes.js";
-import userRouter from "./routes/User.routes.js";
-import orderRouter from "./routes/Order.routes.js";
-import { notFound, errorHandler } from "./Middleware/Errors.js";
+import Express from 'express'
+import dotenv from 'dotenv'
+import connectDatabase from './config/MongoDB.js'
+import ImportData from './DataImport.js'
+import productRouter from './routes/Product.routes.js'
+import userRouter from './routes/User.routes.js'
+import orderRouter from './routes/Order.routes.js'
+import { notFound, errorHandler } from './Middleware/Errors.js'
 
-dotenv.config();
-connectDatabase();
-const app = Express();
-app.use(Express.json());
+dotenv.config()
+connectDatabase()
+const app = Express()
+app.use(Express.json())
 
 // api
 
-app.use("/api/import", ImportData);
-app.use("/api/products", productRouter);
-app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
-app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID);
-});
+app.use('/v1/api/import', ImportData)
+app.use('/v1/api/products', productRouter)
+app.use('/v1/api/users', userRouter)
+app.use('/v1/api/orders', orderRouter)
+app.get('/v1/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID)
+})
 
 // error handler
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
 // // load product from serverside
 // app.get("/api/products", (req, res) => {
@@ -36,10 +36,10 @@ app.use(errorHandler);
 //   res.json(product);
 // });
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
+app.get('/', (req, res) => {
+  res.send('API Running')
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running on port ${PORT}`));
+app.listen(PORT, console.log(`server running on port ${PORT}`))
